8b867febf016cf199d3a774da343adc8d7da5e40
[bogdanro] <gitlab@bogdanr.ro>
2017-11-03 16:34:38 +0200

    test3

---
+++ index.js
@@ -0,0 +1,13 @@
+var express = require('express')
+var app = express()
+
+app.set('port', (process.env.PORT || 8081))
+app.use(express.static(__dirname + '/public_dir'))
+
+app.get('/', function(request, response) {
+  response.send('Hello World, 49yarn')
+})
+
+app.listen(app.get('port'), function() {
+  console.log("Node app is running at localhost:" + app.get('port'))
+})
